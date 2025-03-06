f66f264f924b65ea60f1bf0ffb2ee0519618f251
christophe77 <chrisb@gmx.fr>
2025-03-06 09:51:00 +0100

    added HeadDocument stories

---
+++ src/components/Foundations/HeadDocument/HeadDocument.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from '@storybook/react';
+import HeadDocument from './HeadDocument';
+
+const metaHeadDocument: Meta<typeof HeadDocument> = {
+	title: 'Components/Foundations/HeadDocument',
+	component: HeadDocument,
+	argTypes: {
+		title: { control: 'text', defaultValue: 'eisenhower-ui' },
+		metas: [],
+		links: [],
+	},
+	tags: ['autodocs'],
+};
+
+export default metaHeadDocument;
+
+export const Default: StoryObj<typeof metaHeadDocument> = {};
+
+const metas = [
+    { name: 'description', content: 'Eisenhower ui design system' },
+];
+const links = [
+    { rel: 'stylesheet', href: 'styles.css' },
+    { rel: 'canonical', href: 'https://example.com/page' },
+];
+
+export const WithMetas = () => <HeadDocument metas={metas} links={links}/>;
